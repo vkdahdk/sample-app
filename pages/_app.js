4ccf599e1faf1dc1768a9f059c67da0373dd5c8a
jeseung <testblank@naver.com>
2022-01-10 14:39:37 +0900

    add: AppContext추가
    _app에 conetxt provider component추가

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
 import '../styles/globals.css';
 import { wrapper } from '../redux/configureStore';
+import AppContext from '../AppContext';
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <AppContext.Provider value={''}>
+            <Component {...pageProps} />
+        </AppContext.Provider>
+    );
 }
 
 export default wrapper.withRedux(MyApp);
